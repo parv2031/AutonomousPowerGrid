2ab211b1b391385a11ed7185718d9aa2ca00d052
parv2031 <parv9457027151@gmail.com>
2026-05-03 22:57:45 +0530

    Initial Commit

---
+++ build/CMakeFiles/powergrid_rl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/powergrid_rl.dir/cpp/rough2.cpp.o"
+  "CMakeFiles/powergrid_rl.dir/cpp/rough2.cpp.o.d"
+  "powergrid_rl"
+  "powergrid_rl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/powergrid_rl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/powergrid_rl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for powergrid_rl.
+# This may be replaced when dependencies are built.
